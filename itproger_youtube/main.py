6ef460f0b990bb0a974b0b017c83e8847d713514
gmurad97 <murad.dev@bk.ru>
2024-03-14 07:58:24 +0400

    youtube lesson practice 3

---
+++ itproger_youtube/main.py
@@ -0,0 +1,35 @@
+# print("hello world")
+
+# math_op = 2+2
+# print(math_op)
+
+# math_op = 11-9
+# print(math_op)
+
+# math_op = 2*9
+# print(math_op)
+
+# math_op = 3/2
+# print(math_op)
+
+# math_op = 1/3
+# print(math_op)
+
+# math_op = 2 // 4
+# print(math_op)
+
+# math_op = max(5,1554)
+# print(math_op)
+
+# math_op = min(5,1554)
+# print(math_op)
+
+# import math
+# print(math.sin(math.radians(90)))
+
+# print(3*3+2)
+# print(2+3*3)
+
+# print(sep="separator_for_array",end="endline")
+
+# print("\n\t\b\\....")
